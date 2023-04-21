1445fd46bdf182d297733003a28adbc35325b328
atoka <makrusan@gmail.com>
2023-04-21 22:12:29 +0530

    Fix comment in the script

--- tools/check_code_format.sh
+++ tools/check_code_format.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check code format.
-# Usage: ./check_format.sh
+# Usage: ./tools/check_code_format.sh
 
 find src test -name \*.cpp -or -name \*.hpp | xargs clang-format -output-replacements-xml | grep -q "<replacement " \
   && echo "Code is not formatted correctly" && exit 1 \
